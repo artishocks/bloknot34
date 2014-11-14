2358d35697f1db8a39203deded8201efa6bb4f87
artishocks <eternal_blisss@mail.ru>
2014-11-14 19:31:14 +0300

    я обиделась

--- http/index.php
+++ http/index.php
@@ -23,13 +23,23 @@
 					<span id="big-title">Издательский дом &laquo;Кнауб&raquo;</span><br>
 					<span id="small-title">рекламно-полиграфическая компания</span>
 				</div>
+
+				<div id="main-menu">
+					<ul class="nav nav-pills">
+					  <li class="active"><a href="?c=0">Главная</a></li>
+					  <li><a href="?c=1">Наши услуги</a></li>
+					  <li><a href="?c=2">Технические условия</a></li>
+					  <li><a href="?c=3">Прайс</a></li>
+					  <li><a href="?c=4">Контакты</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div> 
 
 	<?php
 			$path = $_SERVER['DOCUMENT_ROOT']."/include/";
-			$contentArray = array('main', 'prod', 'price', 'techreq', 'contacts');
+			$contentArray = array('main', 'prod', 'techreq', 'price', 'contacts');
 			
 	        $c = 0;
 			if ( !empty( $_GET['c']) && in_array($c, $contentArray) )
@@ -56,14 +66,13 @@
 			</div>
 
 			<p>e-mail: <a href="mailto:bloknot34@inbox.ru">bloknot34@inbox.ru</a> | <a href="http://vk.com/bloknot_34">vk.com/bloknot_34</a></p>
+			<p>Часы работы: с 8:00 до 17:00. Сб, Вc - выходные</p>
 		</div>
 	</div>
-	<div class="container">Часы работы: с 8:00 до 17:00. Сб, Вc - выходные</div>
+	
 	<div class="ftr">
 		<div class="container">
-			<div class="col-xs-12">
-				<p class="text-center">Адрес: 404121, г. Волжский, ул. Горького, 41р</p>
-			</div>
+				
 		</div>
 	</div>
 
